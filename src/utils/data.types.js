df6d14ab2d29254c1d394ec0be23298dfa572e7b
Fabricio Orrala <fabricio.orrala@gmail.com>
2023-12-10 16:26:30 -0500

    Refine GET endpoint for user contracts, filtering non-terminated contracts (#4)
    This commit enhances the GET endpoint for user contracts, ensuring that the returned list exclusively consists of non-terminated contracts. The modification improves the accuracy and relevance of the endpoint's response.
    
    The changes include:
    - Implementation of logic to filter out terminated contracts from the result.
    - Addition of util/types to prevent the hardcoding of strings, contributing to a more maintainable codebase.
    
    These adjustments provide a more precise and reliable representation of a user's contracts.

---
+++ src/utils/data.types.js
@@ -0,0 +1,15 @@
+const ContractTypes = {
+  IN_PROGRESS: "in_progress",
+  NEW: "new",
+  TERMINATED: "terminated"
+};
+
+const ProfileTypes = {
+  CLIENT: "client",
+  CONTRACTOR: "contractor"
+};
+
+module.exports = {
+  ContractTypes,
+  ProfileTypes
+}
--- tests/contracts.test.js
+++ tests/contracts.test.js
@@ -1,4 +1,5 @@
 const supertest = require('supertest');
+const { Contract } = require('../src/model');
 const app = require('../src/app');
 
 describe('GET /contracts/:id', () => {
@@ -21,6 +22,15 @@ describe('GET /contracts/:id', () => {
   });
 
   it('should return 403 if unauthorized', async () => {
+    Contract.findAll = jest.fn(() => {
+      return {
+        id:2,
+        terms: 'bla bla bla',
+        status: 'in_progress',
+        ClientId: 1,
+        ContractorId: 6
+      };
+    });
     const response = await supertest(app)
       .get('/contracts/1')
       .set('profile_id', '2');
@@ -29,13 +39,65 @@ describe('GET /contracts/:id', () => {
   });
 
   it('should return 500 on server error', async () => {
-    const { Contract } = require('../src/model');
     Contract.findOne = jest.fn(() => {
       throw new Error();
     });
 
     const response = await supertest(app)
       .get('/contracts/1')
+      .set('profile_id', '1');
+
+    expect(response.status).toBe(500);
+  });
+});
+
+describe('GET /contracts', () => {
+  it('should return a list of contracts if authorized', async () => {
+    Contract.findAll = jest.fn(() => {
+      return [
+        {
+          id:1,
+          terms: 'bla bla bla',
+          status: 'terminated',
+          ClientId: 1,
+          ContractorId:5
+        },
+        {
+          id:2,
+          terms: 'bla bla bla',
+          status: 'in_progress',
+          ClientId: 1,
+          ContractorId: 6
+        }
+      ];
+    });
+    const response = await supertest(app)
+      .get('/contracts')
+      .set('profile_id', '1');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+
+  it('should return an empty array if the user has no contracts', async () => {
+    Contract.findAll = jest.fn(() => {
+      return [];
+    });
+    const response = await supertest(app)
+      .get('/contracts')
+      .set('profile_id', '3');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+
+  it('should return 500 on server error', async () => {
+    Contract.findAll = jest.fn(() => {
+      throw new Error();
+    });
+
+    const response = await supertest(app)
+      .get('/contracts')
       .set('profile_id', '1');
 
     expect(response.status).toBe(500);
