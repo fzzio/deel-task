4e220aebff95f021c932341cdb1790d45c44bad5
Fabricio Orrala <fzzio007@gmail.com>
2023-12-11 02:59:38 -0500

    Best clients endpoint

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -34,7 +34,7 @@ const bestProfession = async (req, res) => {
 
     if (moneyEarnedByProfession.length > 0) {
       const bestProfession = moneyEarnedByProfession[0].Contract.Contractor.profession;
-      return res.status(200).json({ bestProfession });
+      return res.status(200).json({ profession: bestProfession });
     } else {
       return res.status(404).json({ error: 'No professions found' });
     }
@@ -44,6 +44,60 @@ const bestProfession = async (req, res) => {
   }
 };
 
+const bestClients =  async (req, res) => {
+  const { Contract, Profile, Job } = req.app.get('models');
+  const sequelize = req.app.get('sequelize');
+  const { start, end, limit } = req.query;
+
+  if (!start || !end) {
+    return res.status(400).json({ error: 'Both start and end parameters are required' });
+  }
+
+  try {
+    const moneyPaidByClient = await Job.findAll({
+      attributes: [
+        'Contract.Client.firstName',
+        'Contract.Client.lastName',
+        [sequelize.fn('sum', sequelize.col('price')), 'totalMoneyPaid'],
+      ],
+      where: {
+        paid: true,
+        paymentDate: { [Op.between]: [start, end] },
+      },
+      include: {
+        model: Contract,
+        include: {
+          model: Profile,
+          as: 'Client',
+          where: { type: ProfileTypes.CLIENT },
+        },
+      },
+      group: ['Contract.Client.firstName', 'Contract.Client.lastName'],
+      order: [[sequelize.col('totalMoneyPaid'), 'DESC']],
+      limit: limit || 2,
+    });
+
+
+    if (moneyPaidByClient.length > 0) {
+      return res.status(200).json(
+        moneyPaidByClient.map((client) => {
+          return {
+            id: client.Contract.Client.id,
+            fullName: `${client.Contract.Client.firstName} ${client.Contract.Client.lastName}`,
+            paid: `${client.dataValues.totalMoneyPaid}`
+          };
+        })
+      );
+    } else {
+      return res.status(404).json({ error: 'No clients found' });
+    }
+  } catch (error) {
+    console.error('Error finding best clients:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 module.exports = {
+  bestClients,
   bestProfession
 };
--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -4,5 +4,6 @@ const { getProfile } = require('../middleware/getProfile')
 const adminController = require('../controllers/admin.controller');
 
 router.get('/best-profession', getProfile, adminController.bestProfession);
+router.get('/best-clients', getProfile, adminController.bestClients);
 
 module.exports = router;
